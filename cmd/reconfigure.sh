29ff053e4fbd87408c385cfcf743bf604da9a224
Isham <ishamaraia@gmail.com>
2017-07-30 18:20:25 +0200

    Starting rework on disks management

--- cmd/reconfigure.sh
+++ cmd/reconfigure.sh
@@ -18,8 +18,8 @@ function reconfigure() {
     cpu: 1                    *REQUIRED
     ram: 1024                 *REQUIRED
     sr_name: 'Local storage'  *REQUIRED
-    disk_0: 10GiB             *REQUIRED
-    disk_1: 200MiB            *Optional (but must be incremental, limit: 12)
+    disk_0: 10GiB             *Optional (default value= 10GiB)
+    disk_1: 200MiB            *Optional (default value= 10GiB)
     pub_key:                  *REQUIRED
 ____EOF____
   }
@@ -34,7 +34,6 @@ ____EOF____
 		'cpu' \
 		'ram' \
 		'sr_name' \
-		'disk_0' \
 		'pub_key'
 	
 	#Get storagege Resource uuid
@@ -45,31 +44,18 @@ ____EOF____
 		"Get Storage Resource uuid -> ${sr_uuid}" \
 		"fail" \
 		"${LINENO}"
+	
+	#Get disks info
+	disks=$(printenv | grep disk_[0-8])
+	act_disks=$(xe vm-disk-list uuid=${vm_uuid}  | grep userdevice | awk {'print $4'} | sort);
+	des_disks=$(printenv | grep disk_[0-8] | grep -v '^disk_3=' | 
+		awk -F 'disk_' {'print $2'} | 
+		awk -F '=' {'print $1'} | sort);
+	#[ ] Check disk value format
+	#[ ] Check disk size
+	#[ ] Disk position 3 will be ignored
+	#[ ] Ignore shrink
 
-	#Check disks param format && \
-	#Get desidered disks number
-	for n in {0..8}; do
-		current_value=$(eval echo "\$disk_${n}");
-		if [[ -n "${current_value}" ]]; then
-			major_disk="${n}"
-		else
-			break;
-		fi
-		
-		if [[ $(eval echo "\$disk_${n}") =~ ^[0-9]*GiB$ ]] \
-				|| [[ $(eval echo "\$disk_${n}") =~ ^[0-9]*MiB$ ]]; then
-			log 'msg' "OK: disk_${n} => ${current_value}"
-		else
-			log 'exit' "FAIL: disk_${n} => ${current_value} (not right format)"
-		fi
-
-		if [[ "${n}" -gt '7' ]]; then
-			log "exit" "you're trying to add too much disks."
-		fi
-	done
-
-	#Rebase start point to 1
-	major_disk=$(( ${major_disk} + 1 ))
 
 	#Service messages
 	log 'msg' "OK: Desidered ram -> ${ram}"
@@ -109,34 +95,11 @@ ____EOF____
 		"fail" \
 		"${LINENO}"
 
-	##Get actual values DISKS N
-	##
-	actual_disks=$(xe vm-disk-list uuid=${vm_uuid} |
-		grep VDI | 
-		wc -l);
-	check_exit \
-		"$?" \
-		"Actual disks number is: ${actual_disks}" \
-		"Get actual disks number -> ${actual_disks}" \
-		"fail" \
-		"${LINENO}"
-	
-	#Check disks size changes
-	if [[ "${major_disk}" -gt "${actual_disks}" ]]; then
-		for disk in $(seq 0 ${actual_disks}); do
-			echo disk_${disk}
-		done
-	else
-		for disk in $(seq 0 ${major_disk}); do
-			echo disk_${disk}
-		done
-	fi
 
 	##_Perform Shutdown action only \
 	##_if something is changed
 	if [[ "${actual_ram}" != "${ram}" ]] || \
-		[[ "${actual_cpu}" != "${cpu}" ]] || \
-		[[ "${actual_disks}" != "${major_disk}" ]]; then
+		[[ "${actual_cpu}" != "${cpu}" ]]; then
 			
 			get_vm_current_status=$(xe vm-list uuid=${vm_uuid} \
 				params=power-state  | awk {'print $5'});
@@ -177,83 +140,6 @@ ____EOF____
 					"setup static vCPU: ${cpu}" \
 					"fail" \
 					"${LINENO}"
-			fi
-
-
-			#REMOVE disks as the desidered state
-			if [[ ${actual_disks} -gt ${major_disk} ]]; then
-				#Get how many disk we have to remove.
-				diff=$(( ${actual_disks} - ${major_disk} ));
-				ids=$(seq 0 $(( ${actual_disks} - 1 )) | tail -n ${diff});
-
-				#Print IDs
-				for id in ${ids}; do
-					if [[ ${id} -gt 2 ]]; then
-						device_position=$(( ${id} + 1 ));
-					else
-						device_position=${id};
-					fi
-
-					#Get vbd to remove
-					vbd_to_rm=$(xe vm-disk-list uuid=${vm_uuid} | 
-							grep "userdevice ( RW): ${device_position}" -B 2 | 
-							head -n 1 | 
-							awk {'print $5'});
-
-					#Get vbd to remove
-					vdi_to_rm=$(xe vbd-list uuid=${vbd_to_rm} params=vdi-uuid | 
-						awk {'print $5'});
-
-					#Detroy VBD without any chance to recover it.
-					xe vbd-destroy uuid="${vbd_to_rm}"
-					check_exit \
-						"$?" \
-						"Remove VBD ${vbd_to_rm}" \
-						"Remove VBD ${vbd_to_rm}" \
-						"fail" \
-						"${LINENO}"
-
-					#Detroy VDI without any chance to recover it.
-					xe vdi-destroy uuid="${vdi_to_rm}"
-					check_exit \
-						"$?" \
-						"Remove VDI ${vdi_to_rm}" \
-						"Remove VDI ${vdi_to_rm}" \
-						"fail" \
-						"${LINENO}"
-
-				done
-			fi
-
-			#ADD disks as the desidered state
-			if [[ ${actual_disks} -lt ${major_disk} ]]; then
-			
-
-				#Get how many disk we have to add.
-				diff=$(( ${major_disk} - ${actual_disks} ));
-				ids=$(seq 0 $(( ${major_disk} - 1 )) | tail -n ${diff});
-
-				#Print IDs
-				for id in ${ids}; do
-
-					#Get virtual_size and device to create VDI
-					virtual_size=$(eval echo "\$disk_${id}")
-
-					#Start adding new disks
-					log 'msg' "MSG: Adding => disk_${id} | virtualsize => ${virtual_size}";
-
-					xe vm-disk-add \
-						disk-size="${virtual_size}" \
-						sr-uuid="${sr_uuid}" \
-						vm="${vm_uuid}" \
-						device="autodetect" > /dev/null 2>&1
-					check_exit \
-						"$?" \
-						"Create VDI on sr ${sr_uuid}." \
-						"Create VDI on sr ${sr_uuid}." \
-						"fail" \
-						"${LINENO}"
-				done
 			fi
 
 			#Start VM
